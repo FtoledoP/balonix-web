5b91a8efa6b495a5199c06ca99bd3a4af606b377
Franco Toledo <127957822+FtoledoP@users.noreply.github.com>
2025-09-07 04:25:02 -0300

    Working sidebar

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,4 +12,9 @@ import { RouterModule } from '@angular/router';
 })
 export class SidebarComponent {
 
+  constructor(private sidebarService: SidebarService) { }
+
+  toggleSidebar() {
+    this.sidebarService.toggle();
+  }
 }
