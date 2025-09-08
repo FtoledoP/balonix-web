1e4bb55f434997bf81175a72f042019de123a8d8
Franco Toledo <127957822+FtoledoP@users.noreply.github.com>
2025-09-07 22:02:19 -0300

    cleaned the home .ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FirebaseService } from '../services/firebase.service';
-import { ThemeService } from '../services/theme.service';
-import { Router } from '@angular/router';
-import { signOut } from 'firebase/auth';
 
 @Component({
   selector: 'app-home',
@@ -14,22 +10,6 @@ import { signOut } from 'firebase/auth';
 })
 export class HomeComponent {
 
-  constructor(
-    private firebaseService: FirebaseService,
-    private themeService: ThemeService,
-    private router: Router
-  ) { }
+  constructor() { }
 
-  toggleDarkMode() {
-    this.themeService.toggleDarkMode();
-  }
-
-  async logout() {
-    try {
-      await signOut(this.firebaseService.auth);
-      this.router.navigate(['/login']);
-    } catch (error) {
-      console.error('Error logging out:', error);
-    }
-  }
 }
