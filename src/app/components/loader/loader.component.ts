d235e546f2fe9bb2b6eafe68f6b6dc8f686faed6
Franco Toledo <127957822+FtoledoP@users.noreply.github.com>
2025-09-07 22:01:42 -0300

    loaders finished

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+  styleUrls: ['./loader.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('400ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('400ms ease-out', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class LoaderComponent {}
