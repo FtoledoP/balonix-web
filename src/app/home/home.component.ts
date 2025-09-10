3fc1c97b08dfacd2a3f01a74b628c275c42e460e
Franco Toledo <127957822+FtoledoP@users.noreply.github.com>
2025-09-09 23:40:16 -0300

    Great changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { TeamRegisterModalComponent } from '../components/team-register-modal/team-register-modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TeamRegisterModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isTeamRegisterModalOpen = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  navigateToTeam() {
+    this.router.navigate(['/team-management']);
+  }
+
+  toggleTeamRegisterModal() {
+    this.isTeamRegisterModalOpen = !this.isTeamRegisterModalOpen;
+  }
+
+  navigateToCalendar() {
+    // Implementar cuando exista la ruta de calendario
+    console.log('Navegando al calendario');
+  }
 }
